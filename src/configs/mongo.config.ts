ec2bf5176bc566d2c4b721918cdfa30e4b671dfa
max99xam <pepetrov123@list.ru>
2022-07-13 23:18:26 +0300

    temporary updated db uri config

--- src/configs/mongo.config.ts
+++ src/configs/mongo.config.ts
@@ -10,16 +10,17 @@ export const getMongoConfig = async (
 };
 
 const getMongoString = (configService: ConfigService) =>
-  'mongodb://' +
-  configService.get('MONGO_LOGIN') +
-  ':' +
-  configService.get('MONGO_PASSWORD') +
-  '@' +
-  configService.get('MONGO_HOST') +
-  ':' +
-  configService.get('MONGO_PORT') +
-  '/' +
-  configService.get('MONGO_AUTHDATABASE');
+'mongodb://ux0myu0nuahtdsz3vjy3:JKzG5YUcbb8DfaPl5otA@n1-c2-mongodb-clevercloud-customers.services.clever-cloud.com:27017,n2-c2-mongodb-clevercloud-customers.services.clever-cloud.com:27017/bensvlxryceli4f?replicaSet=rs0';
+  // 'mongodb://' +
+  // configService.get('MONGO_LOGIN') +
+  // ':' +
+  // configService.get('MONGO_PASSWORD') +
+  // '@' +
+  // configService.get('MONGO_HOST') +
+  // ':' +
+  // configService.get('MONGO_PORT') +
+  // '/' +
+  // configService.get('MONGO_AUTHDATABASE');
 
 const getMongoOptions = () => ({
   useNewUrlParser: true,
