118610ff803b27b72e508eba8271092e3e992696
max <kramax42@yandex.by>
2022-08-09 14:40:59 +0300

    updated cors config for production

--- src/main.ts
+++ src/main.ts
@@ -14,9 +14,24 @@ async function bootstrap() {
 
   // Explanation of problem with CORS.
   // https://tutorialmeta.com/question/nest-js-is-giving-cors-error-even-when-cors-is-enabled
+  // app.enableCors({
+  //   allowedHeaders: ['content-type'],
+  //   origin: ['http://localhost:3000', 'https://test-code-knowledge.vercel.app/'],
+  //   credentials: true,
+  // });
+  let whitelist = ['http://localhost:3000', 'https://test-code-knowledge.vercel.app'];
   app.enableCors({
-    allowedHeaders: ['content-type'],
-    origin: ['http://localhost:3000', 'https://test-code-knowledge.vercel.app/'],
+    origin: function (origin, callback) {
+      if (whitelist.indexOf(origin) !== -1) {
+        console.log("allowed cors for:", origin)
+        callback(null, true)
+      } else {
+        console.log("blocked cors for:", origin)
+        callback(new Error('Not allowed by CORS'))
+      }
+    },
+    allowedHeaders: 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe',
+    methods: "GET,PUT,POST,DELETE,UPDATE,OPTIONS",
     credentials: true,
   });
   app.use(helmet());
